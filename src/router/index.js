a9e3bcb9b60062114efb2f6c392aad799f2b054b
谭佳林 <784939830@qq.com>
2018-09-25 14:19:02 +0800

    提交登录文件

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
+const login = r => require.ensure([], () => r(require('@/views/login/index')), 'login');
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: '/login',
+      component: login
     }
   ]
 })
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,11 +1,26 @@
 <template>
 	<div>
-		登录页面
-	</div>
+		  <el-input v-model="input" placeholder="账号"></el-input>
+		  <el-input v-model="input" placeholder="密码"></el-input>
+	 </div>
 </template>
-
 <script>
+export default {
+  data() {
+    return {
+      input: ''
+    }
+  },
+  components: {
+  	
+  },
+  computed: {
+  	
+  },
+  methods: {
+  	
+  }
+}
 </script>
-
 <style>
 </style>
